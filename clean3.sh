2e6fbc5e998aa834ace364b8671443d56d8e0f7e
pavelkrolevets <pavelkrolevets@gmail.com>
2020-08-25 14:24:41 +0600

    modified:   .gitignore 	modified:   clean3.sh 	modified:   genesis.json 	deleted:    node1/static-nodes.json 	deleted:    node2/static-nodes.json 	deleted:    node3/static-nodes.json 	modified:   run1.sh 	new_node/ 	node_new/ 	run_new.sh

--- clean3.sh
+++ clean3.sh
@@ -2,8 +2,11 @@
 
 
 rm -rf ./node1/geth/
+echo "Cleaned node1"
 rm -rf ./node2/geth/
+echo "Cleaned node2"
 rm -rf ./node3/geth/
+echo "Cleaned node3"
 
 ./geth --datadir node1/ init genesis.json
 ./geth --datadir node2/ init genesis.json
